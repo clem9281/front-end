9cd9f993a6c10c374b83db32f2889aa4a47d38d4
clem9281 <clem9281@gmail.com>
2019-03-12 21:48:26 -0700

    added responsive nav

--- bubl/src/components/NavBar.js
+++ bubl/src/components/NavBar.js
@@ -3,10 +3,30 @@ import bublLogo from "../assets/bubl-logo.png";
 import { withRouter, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import { logOut } from "../actions";
+import DropDown from "./DropDown";
 
 class NavBar extends Component {
-  state = {};
-
+  state = {
+    width: null,
+    dropdown: false
+  };
+  componentDidMount() {
+    this.setState({ width: window.innerWidth });
+    window.addEventListener("resize", this.handleResize);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+  handleResize = () => {
+    this.setState({ width: window.innerWidth });
+    if (window.innerWidth > 500) {
+      this.setState({ dropdown: false });
+    }
+  };
+  handleMenuClick = e => {
+    e.preventDefault();
+    this.setState({ dropdown: !this.state.dropdown });
+  };
   logOut = e => {
     this.props.logOut();
     this.props.history.push("/login");
@@ -20,14 +40,23 @@ class NavBar extends Component {
             <img src={bublLogo} alt="" />
           </div>
           <div className="links">
-            <NavLink exact to="/bubls">
-              My Bubls
-            </NavLink>
-            <NavLink exact to="/">
-              Profile
-            </NavLink>
-            <button onClick={this.logOut}> Log Out </button>
+            {this.state.width > 500 ? (
+              <>
+                <NavLink exact to="/bubls">
+                  My Bubls
+                </NavLink>
+                <NavLink exact to="/">
+                  Profile
+                </NavLink>
+                <button onClick={this.logOut}> Log Out </button>
+              </>
+            ) : (
+              <button onClick={this.handleMenuClick}>
+                <i className="fas fa-bars fa-2x" />
+              </button>
+            )}
           </div>
+          {this.state.dropdown && <DropDown />}
         </nav>
       </header>
     );
